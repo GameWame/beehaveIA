df3c23e8049587abc83162b90936d4ec1502a8df
MariaLuciaFede01 <114806100+MariaLuciaFede01@users.noreply.github.com>
2023-02-07 17:12:37 +0100

    main

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from CreateDataset import IMG_SIZE
 from Net import Net
 from cnn import train
 
-#net = train()
-#torch.save(net.state_dict(), "./VarroaModel.pth")
+net = train()
+torch.save(net.state_dict(), "./VarroaModel.pth")
 
 img = cv2.imread("SiVarroa/0CRzEsP2ONDJtOkqP8fSSE5Dif7iaVoB.jpeg")
 bgr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 resized = cv2.resize(bgr, IMG_SIZE)
 
 net = Net()
-net.load_state_dict(torch.load("VarroaModel.pth"))
+net.load_state_dict(torch.load("VarroaModel.pth"), strict=False)
 net.eval()
-result = net(Tensor(resized))
+result = net(Tensor(resized).view(-1, 1, IMG_SIZE[0], IMG_SIZE[1]))
 print(result)
