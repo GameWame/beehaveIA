f1e6c2bc8fd7c2c5c79d470c10a10ba4ee23d320
Luigi11162 <luigibacco01@gmail.com>
2023-02-09 11:20:56 +0100

    Removed last unnecessary convolutional layer

--- Net.py
+++ Net.py
@@ -7,14 +7,13 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2, stride=1)
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.bn4 = nn.BatchNorm2d(512)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=2, stride=1)
+
+        self.bn1 = nn.BatchNorm2d(128)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.bn3 = nn.BatchNorm2d(512)
         self.fc1 = nn.Linear(512, 2)
 
     def forward(self, x):
@@ -25,7 +24,6 @@ class Net(nn.Module):
 
         x = F.max_pool2d(F.relu(self.bn3(self.conv3(x))), 3)
 
-        x = F.relu(self.bn4(self.conv4(x)))
 
         to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
         x = x.view(-1, to_linear)
